1be0e9382dc48180a738f0a8d235382f27232dbb
Mehmet Kanat <mehmet.kanat@protonmail.com>
2020-10-06 00:36:30 +0300

    Change dice by clicking roll button

--- dicee/Dicee-iOS13/ViewController.swift
+++ dicee/Dicee-iOS13/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageViewLeft: UIImageView!
+    @IBOutlet weak var diceImageViewRight: UIImageView!
+    
+    var leftDiceNumber = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageViewLeft.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        leftDiceNumber += 1
+        
+        if(leftDiceNumber == 6){
+            leftDiceNumber = 0
+        }
+    }
+    
 }
-
