f27023b574c746b170cf319c316b65ac63d2a753
Mehmet Kanat <mehmet.kanat@protonmail.com>
2020-10-06 23:10:55 +0300

    Completed project

--- dicee/Dicee-iOS13/ViewController.swift
+++ dicee/Dicee-iOS13/ViewController.swift
@@ -12,26 +12,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageViewLeft: UIImageView!
     @IBOutlet weak var diceImageViewRight: UIImageView!
     
-    var leftDiceNumber = 1
-    var rightDiceNumber = 5
+    let dices = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageViewLeft.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
-        diceImageViewRight.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
-        leftDiceNumber += 1
-        rightDiceNumber -= 1
         
-        if(rightDiceNumber == 0){
-            rightDiceNumber = 5
-        }
-        
-        if(leftDiceNumber == 6){
-            leftDiceNumber = 0
-        }
+        diceImageViewLeft.image = dices.randomElement()
+        diceImageViewRight.image = dices.randomElement()
+               
     }
     
 }
