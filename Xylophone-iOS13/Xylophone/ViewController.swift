f1a6ff0df8dcaefab0132d6c89d77c7e72a8ed18
Mehmet Kanat <mehmet.kanat@protonmail.com>
2020-10-19 23:46:20 +0300

    Add challenge #82 solution

--- Xylophone-iOS13/Xylophone/ViewController.swift
+++ Xylophone-iOS13/Xylophone/ViewController.swift
@@ -18,7 +18,15 @@ class ViewController: UIViewController {
     }
     
     @IBAction func keyPressed(_ sender: UIButton) {
+        
+        sender.alpha = 0.5
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3 ) {
+            sender.alpha = 1.0
+        }
+        
         playSound(soundName: sender.currentTitle!)
+        
     }
     
     func playSound(soundName: String) {
